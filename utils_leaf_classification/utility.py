9580a849f2bd42741e267554f1b04ee6261eaf2a
cacad101 <gbbpentium@gmail.com>
2018-04-03 03:21:46 +0800

    add setting utils

--- utils_leaf_classification/utility.py
+++ utils_leaf_classification/utility.py
@@ -1,8 +1,18 @@
 """ Utility """
 
 import datetime
+import json
 import logging
 import os
+
+class DictClass:
+    """ Creating class from dictionary """
+    def __init__(self, dictionary):
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                setattr(self, key, DictClass(value))
+            else:
+                setattr(self, key, value)
 
 def init_logger(log_dir, file_name, level):
     """ Initialise logger """
@@ -21,5 +31,15 @@ def get_now_str():
     return now.strftime("%Y-%m-%d_%H-%M")
 
 def ensure_dir(file_dir):
+    """ Ensure the directory exist """
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
+
+def load_settings(file_path="settings.json", detail=""):
+    """ load settings """
+    settings = {}
+    with open(file_path, 'r') as input_file:
+        settings = json.load(input_file)
+    if detail != "":
+        return DictClass(settings[detail])
+    return DictClass(settings)
