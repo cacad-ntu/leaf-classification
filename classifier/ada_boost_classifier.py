feaab82d7fbcb0d7fb9cb74e8a5fe4693be02a91
StupidDummy <kelvinchandra54@gmail.com>
2018-04-06 02:19:06 +0800

    update ada

--- classifier/ada_boost_classifier.py
+++ classifier/ada_boost_classifier.py
@@ -4,6 +4,13 @@ import logging
 
 from sklearn.ensemble import AdaBoostClassifier
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import NearestNeighbors
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
 from utils_leaf_classification.data_loader import DataLoader
 from utils_leaf_classification.data_selector import DataSelector
 from utils_leaf_classification.k_fold import ModelSelector
@@ -40,17 +47,19 @@ def main():
     ds4 = DataSelector(dl.id_train, dl.x_train, dl.y_train, dl.id_test, dl.x_test)
     ds4.add_all("texture")
 
-    clf = AdaBoostClassifier(learning_rate=0.05)
-    clf_2 = AdaBoostClassifier(learning_rate=0.001)
-    clf_4 = AdaBoostClassifier(learning_rate=0.01)
+    clf_1 = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(), algorithm='SAMME',learning_rate=0.01)
+    clf_3 = AdaBoostClassifier(base_estimator=RandomForestClassifier(), algorithm='SAMME',learning_rate=0.01)
+    clf_6 = AdaBoostClassifier(base_estimator=GaussianNB(), algorithm='SAMME',learning_rate=0.01)
+    clf_7 = AdaBoostClassifier(learning_rate=0.01)
     ms = ModelSelector()
     ms.add_selector("all_features",ds)
     ms.add_selector("only_margin",ds2)
     ms.add_selector("only_shape",ds3)
     ms.add_selector("only_texture",ds4)
-    ms.add_classifier("ada_test",clf)
-    ms.add_classifier("ada_test2",clf_2)
-    ms.add_classifier("ada_test4",clf_4)
+    ms.add_classifier("ada_test1",clf_1)
+    ms.add_classifier("ada_test3",clf_3)
+    ms.add_classifier("ada_test6",clf_6)
+    ms.add_classifier("ada_test7",clf_7)
     ms.get_best_model(k=10)
     ms.generate_submission(settings.data.submission_dir, dl.classes)
 
