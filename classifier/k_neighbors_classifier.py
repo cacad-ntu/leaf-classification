7af0155bc788f86c9b8a60423536e6c006422a9d
cacad101 <gbbpentium@gmail.com>
2018-04-05 22:49:23 +0800

    add best k neighbors

--- classifier/k_neighbors_classifier.py
+++ classifier/k_neighbors_classifier.py
@@ -18,21 +18,96 @@ def main():
     dl.load_test(settings.data.test_path)
     dl.load_train(settings.data.train_path)
 
-    ds = DataSelector()
-    ds.set_train_id(dl.id_train)
-    ds.set_x_train(dl.x_train)
-    ds.set_y_train(dl.y_train)
-    ds.set_test_id(dl.id_test)
-    ds.set_x_test(dl.x_test)
-    ds.add_all()
-
-    clf = KNeighborsClassifier(3)
-    clf_2 = KNeighborsClassifier(6)
-
     ms = ModelSelector()
-    ms.add_selector("ds_1", ds)
-    ms.add_classifier("KNN_k3", clf)
-    ms.add_classifier("KNN_k6", clf_2)
+
+    # Add Data Selector
+    ds = DataSelector(
+        dl.id_train, dl.x_train, dl.y_train,
+        dl.id_test, dl.x_test
+    )
+    ds.add_all()
+    ms.add_selector("all_feature", ds)
+
+    # ds2 = DataSelector(
+    #     dl.id_train, dl.x_train, dl.y_train,
+    #     dl.id_test, dl.x_test
+    # )
+    # ds2.add_all('margin')
+    # ms.add_selector("margin_only", ds)
+
+    # ds3 = DataSelector(
+    #     dl.id_train, dl.x_train, dl.y_train,
+    #     dl.id_test, dl.x_test
+    # )
+    # ds3.add_all('shape')
+    # ms.add_selector("shape_only", ds)
+
+    # ds4 = DataSelector(
+    #     dl.id_train, dl.x_train, dl.y_train,
+    #     dl.id_test, dl.x_test
+    # )
+    # ds4.add_all('texture')
+    # ms.add_selector("texture_only", ds)
+
+    # ds5 = DataSelector(
+    #     dl.id_train, dl.x_train, dl.y_train,
+    #     dl.id_test, dl.x_test
+    # )
+    # ds5.add_all('texture')
+    # ds5.add_all('shape')
+    # ms.add_selector("texture_shape", ds5)
+
+    # ds6 = DataSelector(
+    #     dl.id_train, dl.x_train, dl.y_train,
+    #     dl.id_test, dl.x_test
+    # )
+    # ds6.add_all('texture')
+    # ds6.add_all('margin')
+    # ms.add_selector("texture_margin", ds6)
+
+    # ds7 = DataSelector(
+    #     dl.id_train, dl.x_train, dl.y_train,
+    #     dl.id_test, dl.x_test
+    # )
+    # ds7.add_all('margin')
+    # ds7.add_all('shape')
+    # ms.add_selector("margin_shape", ds7)
+
+    # Add Classifier
+    # clf = KNeighborsClassifier(2)
+    # ms.add_classifier("k_2", clf)
+
+    # clf_2 = KNeighborsClassifier(4)
+    # ms.add_classifier("k_4", clf_2)
+
+    # clf_3 = KNeighborsClassifier(6)
+    # ms.add_classifier("k_6", clf_3)
+
+    # clf_4 = KNeighborsClassifier(10)
+    # ms.add_classifier("k_10", clf_4)
+
+    # clf_5 = KNeighborsClassifier(10, weights="distance")
+    # ms.add_classifier("k_10_distance", clf_5)
+
+    # clf_6 = KNeighborsClassifier(10, p=1)
+    # ms.add_classifier("k_10_p1", clf_6)
+
+    # clf_7 = KNeighborsClassifier(10, weights="distance", p=1)
+    # ms.add_classifier("k_10_distance_p1", clf_7)
+
+    # for i in range(10,20):
+    #     clf_k = KNeighborsClassifier(i, weights="distance", p=1)
+    #     ms.add_classifier("k_{}_distance_p1".format(i), clf_k)
+    clf_11 = KNeighborsClassifier(11, weights="distance", p=1)
+    ms.add_classifier("k_11_distance_p1", clf_11)
+
+    # clf_7 = KNeighborsClassifier(10, weights="distance", p=3)
+    # ms.add_classifier("k_10_distance_p3", clf_7)
+
+    # clf_5 = KNeighborsClassifier(20)
+    # ms.add_classifier("k_20", clf_5)
+
+    # Get best model
     ms.get_best_model(k=10)
     ms.generate_submission(settings.data.submission_dir, dl.classes)
 
